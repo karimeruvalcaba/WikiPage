4709bfe3e7eb9381cfc319a1141a4231a4879dfd
karimeruvalcaba <A01656188@tec.mx>
2024-03-26 10:43:27 -0600

    Adding OG files

---
+++ WikiPage/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#TimelineButton").click(function() {
+        displayCrimeTimeline();
+    });
+
+    $("#SpanishButton").click(function() {
+        translateToSpanish();
+    });
+});
