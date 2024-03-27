7cffd50cb9cd3a1c9964b850b59a4a6358881032
karimeruvalcaba <A01656188@tec.mx>
2024-03-27 09:50:33 -0600

    lol. js was missing and some html content

---
+++ WikiPage/index2.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#toggleButton").click(function() {
+        toggleLanguage();
+    });
+
+    $("#switchButton").click(function() {
+        switchLanguage();
+    });
+});
