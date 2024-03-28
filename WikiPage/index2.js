616acb573d963d59c114c51f6ae0869b9dbb2f5e
karimeruvalcaba <A01656188@tec.mx>
2024-03-28 10:38:12 -0600

    finally endpoint using GET DONE

--- WikiPage/index2.js
+++ WikiPage/index2.js
@@ -7,12 +7,21 @@ $(document).ready(function() {
         switchLanguage();
     });
 
+    
+    //$("#GETButton").click(function(){
+
+        //$.get("http://127.0.0.1:5000/getTest",function(data,status){
+            //$("#parrafo-1").html(data);
+
+        //});
+
+    //})
+
+    
     $("#GETButton").click(function(){
-
-        $.get("http://127.0.0.1:5000/getTest",function(data,status){
-            $("#parrafo-1").html(data);
-
+        $.get("http://127.0.0.1:5000/getTest", function(response){
+            $("#GETButton").html(response);
         });
-
-    })
+    });
+    
 });
